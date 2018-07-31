fd2b0a787df726f6984c959b0b6cba440dc32771
Yuta Suzuki; hacone <esperhacone@gmail.com>
2018-07-31 15:59:00 +0900

    detected slippy components and SNVs local to them.

--- Layout.py
+++ Layout.py
@@ -1,8 +1,8 @@
 # This is evolved from Alignments.py, which is then evolved from SNV_distribution.py
-
 from collections import Counter
 from collections import namedtuple
 import numpy as np
+import pickle
 
 from EncodedRead import *
 from HOR_segregation import *
@@ -56,6 +56,20 @@ def valid_read_regions(hers):
             lh = h
         regs[i] = [ l for l in regs[i] if l ] # remove if empty
     return { k : v for k, v in regs.items() if v } # remove if empty
+
+def get_bits_dict(snvs, regions):
+    """ construct bit vectors in dict """
+    bits_dict = {}
+    for i, ai, l in [ (i, ai, pool.arrs[i][ai]) for i, ai in regions ]:
+        v = []
+        for h in l:
+            if h == -1:
+                v += [0] * len(snvs)
+            else:
+                # NOTE: Was snv a dict ? # TODO: cleanup
+                v += [ 1 if any([ (sp.pos, sp.base) == (p, b) for sp in pool.hers[i].mons[h+k].monomer.snvs ]) else -1 for k, p, b, f in snvs ]
+        bits_dict[(i, ai)] = np.array(v).reshape(len(l), len(snvs))
+    return bits_dict
 
 def calc_align(i, ai, j, aj, k, bits_dict):
 
@@ -113,134 +127,108 @@ def print_align(aln, bits_dict):
     # the remaining dangling part if any
     for i in range(re, aln.li):
         lines += f"\n{i:>3}\t" + "".join([ bit_to_char[e] for e in r[i,:] ]) + f"\t***"
-
     for i in range(qe, aln.lj):
         lines += f"\n***\t" + "".join([ bit_to_char[e] for e in q[i,:] ]) + f"\t{i:<3}"
 
     print(lines)
 
+def double_edge_component(alns_dict):
+    """
+    obtain so called slippy component, whose elements are connected by multi-edges.
+    edge filter is hard-coded to be at least 5 units with 50% identity. TODO: see the distribution to support this!
+    """
 
-def layout(alns_pers):
-    """ implementing layout idea; alns_pers = initial pairwise alignment, for now. """
+    def is_double(alns):
+        return 1 < len([ aln for aln in alns if aln.score > 500 and aln.len_ovlp > 4 ])
+
+    nodes = set()
+    for (i, ai), d in alns_dict.items():
+        nodes |= { (i, ai) } | { (j, aj) for (j, aj), alns in d.items() }
+    print(f"{len(nodes)} nodes in total.")
+
+    components = []
+    while len(nodes) > 0:
+        k, ak = nodes.pop()
+        print(f"\nchecking component for {k}-{ak} / {len(nodes)} remain to be checked.")
+
+        curr_component = { (k, ak) } | { (j, aj) for (j, aj), alns in alns_dict[(k, ak)].items() if is_double(alns) }
+        curr_checked = set()
+
+        while len(curr_checked) < len(curr_component):
+            l, al = (curr_component - curr_checked).pop()
+            curr_checked |= { (l, al) }
+            curr_component |= { (j, aj) for (j, aj), alns in alns_dict[(l, al)].items() if is_double(alns) }
+
+        components += [[ e for e in curr_component ]]
+        nodes -= curr_component
+        print(f"{len(curr_component)} nodes in the component")
+
+    return components
+
+def layout(alns_dict = None):
+    """ implementing layout idea """
 
     # setup contexts
     bag_of_units = [ (ri, h) for ri, er in enumerate(pool.hers) for h, _, t in er.hors if t == "~" ]
     n_units = len(bag_of_units)
     print(f"{n_units} units found in {len(pool.hers)} reads.")
+
     snv_sites = detect_snvs(bag_of_units)
-    print(f"{len(snv_sites)} SNV sites defined.")
+    print(f"{len(snv_sites)} SNV sites defined globally.")
+
     arrs = valid_read_regions(pool.hers)
     print(f"{len(arrs)} read has an array to be aligned.")
-    print(f"{ sum([ len(v) for k, v in arrs.items() ]) } arrays in total.")
+    print(f"{ sum([ len(v) for k, v in arrs.items() ]) } in total.")
 
-    # construct bit vectors in dict
-    bits_dict = {}
-    for i, ai, l in [ (i, ai, l) for i, a in arrs.items() for ai, l in enumerate(a) if len(l) > 4 ]:
-        v = []
-        for h in l:
-            if h == -1:
-                v += [0] * len(snv_sites)
-            else:
-                # NOTE: Was snv a dict ? # TODO: cleanup
-                v += [ 1 if any([ (sp.pos, sp.base) == (p, b) for sp in pool.hers[i].mons[h+k].monomer.snvs ]) else -1 for k, p, b, f in snv_sites ]
-        bits_dict[(i, ai)] = np.array(v).reshape(len(l), len(snv_sites))
+    pool = Pool(hers = hers, arrs = arrs) # NOTE: I wanted to express global variables ...
+
+    # bits vectors are constructed for all regions, on SNVs defined globally.
+    regs = [ (i, ai) for i, a in pool.arrs.items() for ai, l in enumerate(a) if len(l) > 4 ]
+    bits_dict = get_bits_dict(snv_sites, regs)
 
     # calculate alignments globally
     long_arrays = [ (i, ai) for i, a in arrs.items() for ai, l in enumerate(a) if len(l) > 9 ]
-    alns_dict = dict()
-    n = 0
-    for i, ai in long_arrays:
-        n += 1
-        print(f"aligning {i} - {ai}. {n} / {len(long_arrays)}")
-        alns_dict[(i, ai)] = dict()
-        for j, aj in long_arrays:
-            if (i, ai) == (j, aj):
-                continue
-            li, lj = bits_dict[(i, ai)].shape[0], bits_dict[(j, aj)].shape[0]
-            # with at least 2 units in overlap
-            alns_dict[(i, ai)][(j, aj)] = sorted([ calc_align(i, ai, j, aj, k, bits_dict = bits_dict) for k in range(-lj+2, li-2) ], key = lambda x: -1 * x.score)
 
-        print(f"{ len([ (j, aj, aln) for (j, aj), alns in alns_dict[(i, ai)].items() for aln in alns if aln.score > 500 and aln.eff_ovlp > 4 ]) } targets found.")
+    # TODO: once again abstract the logic to calc all-vs-all alns_dict for subset of reads (i mean, slippy parts).
+    if not alns_dict:
+        alns_dict = dict()
+        n = 0
+        for i, ai in long_arrays:
+            n += 1
+            print(f"aligning {i} - {ai}. {n} / {len(long_arrays)}")
+            alns_dict[(i, ai)] = dict()
+            for j, aj in [ (j, aj) for j, aj in long_arrays if not (j, aj) == (i, ai)]:
+                li, lj = bits_dict[(i, ai)].shape[0], bits_dict[(j, aj)].shape[0]
+                # with at least 2 units in overlap
+                alns = [ calc_align(i, ai, j, aj, k, bits_dict = bits_dict) for k in range(-lj+2, li-2) ]
+                alns = [ aln for aln in alns if aln.score > 400 and aln.eff_ovlp > 2 ]
+                if alns:
+                    alns_dict[(i, ai)][(j, aj)] = sorted(alns, key = lambda x: -1 * x.score)
+            print(f"{ len([ (j, aj, aln) for (j, aj), alns in alns_dict[(i, ai)].items() for aln in alns if aln.score > 400 and aln.eff_ovlp > 2 ]) } targets found.")
 
-    import pickle
-    pickle.dump(alns_dict, open(f"alns_dict.10u.pickle", "wb"))
+        pickle.dump(alns_dict, open(f"alns_dict.10u.400-2.pickle", "wb"))
+
+    slippies = double_edge_component(alns_dict)
+    print(f"{ len(slippies) } components found.")
+    print(slippies)
+
+    for ic, comp in enumerate(slippies):
+
+        if len(comp) < 2:
+            continue
+        units_in_comp = [ (i, mi) for i, ai in comp for mi in arrs[i][ai] if mi > -1 ]
+
+        snvs = detect_snvs(units_in_comp)
+        print(f"\n{len(snvs)} SNVs for the Component {ic}: {len(comp)} reads; {len(units_in_comp)} units.")
+
+        for k, p, b, f in sorted(snvs):
+            print(f"{k}\t{p}\t{b}\t{f:.2f}")
 
     import sys
     sys.exit()
 
-    goods = [ (j, aj, aln) for (j, aj), alns in alns_dict[long_arrays[0]].items() for aln in alns if aln.score > 600 ]
-
-    print(len(goods))
-    print(goods)
-    print(goods[0])
-
-    for i in range(5):
-        print_align(goods[i][2], bits_dict)
-
-    import sys
-    sys.exit()
-    
-    pw_aln = pickle.load(open(alns_pers, "rb"))
-    print(f"{ sum([ len(pw_aln[i]) for i in pw_aln.keys() ]) } alignments for {len(pw_aln)} reads loaded.")
-
-    def Fx(x):
-        """ X to S
-            scoring scheme: n11 / (n10+n01+n11) """
-        #assert bv1.shape == bv2.shape, "wrong shapes"
-        #m = np.multiply(bv1, bv2) # 1: match, 0: masked, -1: mismatch
-        #mt = np.multiply((m + 1), m) / 2 # 1: match, 0: masked/mismatch
-        #n_m = np.sum(np.multiply(m, m) - mt) # n01 or n10
-        #n11 = np.sum(np.multiply(mt, (bv1 + 1) / 2)) # n11
-        #return int(1000 * n11 / (1 + n11 + n_m)) # per-mille !
-        return 0
-
-    def aln_x(i, j, k):
-        # TODO: memoize ?
-        li, lj = [ bms[x].shape[0] for x in [i, j] ]
-    
-        if k < 0:
-            x1, x2 = bms[i][0:min([li, k+lj]),3:], bms[j][-k:min([lj, -k+li]),3:]
-        else:
-            x1, x2 = bms[i][k:min([li, k+lj]),3:], bms[j][ 0:min([lj, -k+li]),3:]
-
-        m = np.multiply(x1, x2) # 1: match, 0: masked, -1: mismatch
-        #mt = np.multiply((m + 1), m) / 2 # 1: match, 0: masked/mismatch
-        #n_m = np.sum(np.multiply(m, m) - mt) # n01 or n10
-        #n11 = np.sum(np.multiply(mt, (bv1 + 1) / 2)) # n11
-        #return int(1000 * n11 / (1 + n11 + n_m)) # per-mille !
-
-        #return Fx(x), aln_len
-        return 0
-
-    layout = []
-
-    def get_slippy():
-        """ temporarily I'm using context """
-
-        def flatten(l):
-            from itertools import chain
-            return list(chain.from_iterable(l))
-
-        def tr_ext(r): # one-step transitive extension # OBVIOUSLY TOO SLOW
-            # reflexive and symmetric
-            x = [ [ (i, k) for j2, k in r if j == j2 ] for i, j in r ]
-            r = { (i, j) for i, j in flatten(x) }
-            r |= { (j, i) for i, j in r } | { (i, i) for i, j in r } | { (j, j) for i, j in r }
-            return r
-
-        r0 = { (i, j) for i, v in pw_aln.items() for j, alns in v.items() if len([0 for a in alns if a[4] > 500 and a[5] > 4]) > 1 }
-
-
-
-        n_r0 = -1
-        while n_r0 < len(r0):
-            n_r0 = len(r0)
-            print(len(r0))
-            r0 = tr_ext(r0)
-
-    get_slippy()
-
-    #r1 = { (i, j) for i in Set(r0.flatten(
+    # TODO: I need to define layout, alignment between layout/read vs layout/read, check for internal consistency, ... then final iterative assembly procedure.
+    #layout = []
 
 if __name__ == '__main__':
 
@@ -263,10 +251,11 @@ if __name__ == '__main__':
     if args.action == "layout":
         assert args.hors, "need HOR-encoded reads"
         hers = pickle.load(open(args.hors, "rb"))
-        pool = Pool(hers = hers, arrs = None)
+        pool = Pool(hers = hers, arrs = None) # NOTE: I wanted to express global variables ...
         if args.alns:
-            layout(args.alns)
-        #else:
-        #    layout()
+            alns_dict = pickle.load(open(args.alns, "rb"))
+            layout(alns_dict)
+        else:
+            layout()
     else:
         assert None, "invalid action."
