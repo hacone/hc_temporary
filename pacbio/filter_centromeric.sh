ba7896f514895c2d610ede7c3aba652a43fc94e4
Yuta Suzuki; hacone <esperhacone@gmail.com>
2018-12-11 14:17:18 +0900

    tidy filter-centromeric

--- pacbio/filter_centromeric.sh
+++ pacbio/filter_centromeric.sh
@@ -4,6 +4,9 @@
 export SQUEAKR_DIR=../squeakr
 export READ_SQ_DIR=../read_squeakr
 
+export FASTQ_DIR=Fastq
+export TARGET_DIR=Centro_Fasta_143Cells
+
 # shared parameters for squeakr
 export K=6
 export S=20
@@ -11,6 +14,7 @@ export S=20
 export MONS_CQF=./d0.fq.K$K.S$S.ser
 
 make_ref_cqf() {
+
   # NOTE: executed on ax02
 	${SQUEAKR_DIR}/squeakr-count -f -k $K -s $S -t 1 -o ./ ../data/monomers/d0.fq \
 	&& mv d0.fq.ser ${MONS_CQF}
@@ -20,26 +24,25 @@ make_ref_cqf() {
 filter_centromeric() {
 
 	READS_FQ=$1
-
 	# TODO: check here whether the result is already exsited
-
 	## This is the call for read-squeakr
+
 	# -- for plain fq
-	# ${READ_SQ_DIR}/squeakr-count -f -k $K -s $S -r ${MONS_CQF} -t 1 -o . ${READS_FQ}
+	# ${READ_SQ_DIR}/squeakr-count -f -k $K -s $S -r ${MONS_CQF} -t 1 -o . ${READS_FQ} > ...
+
 	# -- for fq.gz
-	${READ_SQ_DIR}/squeakr-count -g -k $K -s $S -r ${MONS_CQF} -t 1 -o . Fastq/${READS_FQ} > Centro_Fasta_143Cells/${READS_FQ%%.fastq.gz}.fasta \
-	&& gzip Centro_Fasta_143Cells/${READS_FQ%%.fastq.gz}.fasta
-	# there might be better way
+	${READ_SQ_DIR}/squeakr-count \
+        -g -k $K -s $S -r ${MONS_CQF} -t 1 -o . ${FASTQ_DIR}/${READS_FQ} \
+        > ${TARGET_DIR}/${READS_FQ%%.fastq.gz}.fasta \
+	&& gzip ${TARGET_DIR}/${READS_FQ%%.fastq.gz}.fasta
 
 	echo "done for $READS_FQ"
-
 }; export -f filter_centromeric
 
-mkdir -p Centro_Fasta_143Cells
-
-#ls Fastq/ | grep .fastq.gz \
-#| xargs -P 8 -I % bash -c "filter_centromeric %"
-
 make_ref_cqf
+
+mkdir -p $TARGET_DIR
+ls $FASTQ_DIR | grep .fastq.gz \
+| xargs -P 8 -I % bash -c "filter_centromeric %"
 
 echo "all done"
