9c23f955c3cdb5d9e5f51b78690e5f5b06471e19
Yuta Suzuki; hacone <esperhacone@gmail.com>
2019-05-20 14:56:13 +0900

    use only triangulated edges

--- Edges2Layout.py
+++ Edges2Layout.py
@@ -26,12 +26,11 @@ from UnitAnalysis import *
 
 def describe(G, name = "Graph"):
     tcycles, icycles = transitive_cycles(G, verbose = False)
-    simples = simple_nodes(G)
-    plus_dang, minus_dang = dang_nodes(G, True), dang_nodes(G, False)
+    #simples = simple_nodes(G)
+    #plus_dang, minus_dang = dang_nodes(G, True), dang_nodes(G, False)
     clens = sorted([ len(comp) for comp in nx.weakly_connected_components(G) ], reverse = True)
     print(f"{name:15}\t{len(G.nodes)}\t{clens[0]} ({100* clens[0] / sum(clens):.1f})\t" +\
-          f"{len(G.edges)}\t{len(simples)}\t" +\
-          f"{len(plus_dang)}\t{len(minus_dang)}\t{len(tcycles)}\t{len(icycles)}")
+          f"{len(G.edges)}\t{len(tcycles)}\t{len(icycles)}")
 
 def largest_component(G):
     ln = sorted([ (len(comp), comp) for comp in nx.weakly_connected_components(G) ],
@@ -149,72 +148,6 @@ def transitive_cycles(G, verbose = True):
               f"{len(set([ (a,c) for a,b,c in transitive_cycles ]))} reducible edges." )
     return (transitive_cycles, contradictory_cycles)
 
-# NOTE: deprecate
-def dang_nodes(G, plus = True):
-    """ pick nodes with degree 1, which are then reduced """
-    if plus:
-        return [ n for n in list(G.nodes) if G.in_degree(n) == 1 and G.out_degree(n) == 0 ]
-    else:
-        return [ n for n in list(G.nodes) if G.in_degree(n) == 0 and G.out_degree(n) == 1 ]
-
-# NOTE: deprecate
-def chop_nodes(G, ns, plus = True):
-    """ trim nodes found by dang_nodes """
-    for n in ns:
-        if plus:
-            b = list(G.in_edges(n, keys = True, data = True))[0]
-            if "dang" in G.nodes[b[0]]:
-                G.nodes[b[0]]["dang"] += [(n, b[2])]
-            else:
-                G.nodes[b[0]]["dang"] = [(n, b[2])]
-            if "dang" in G.nodes[n]:
-                G.nodes[b[0]]["dang"] += [ (m, b[2] + k) for m, k in G.nodes[n]["dang"] ]
-            G.remove_edge(b[0], n)
-
-        else:
-            b = list(G.out_edges(n, keys = True, data = True))[0]
-            if "dang" in G.nodes[b[1]]:
-                G.nodes[b[1]]["dang"] += [(n, -b[2])]
-            else:
-                G.nodes[b[1]]["dang"] = [(n, -b[2])]
-            if "dang" in G.nodes[n]:
-                G.nodes[b[1]]["dang"] += [ (m, -b[2] + k) for m, k in G.nodes[n]["dang"] ]
-            G.remove_edge(n, b[1])
-        G.remove_node(n)
-
-# NOTE: deprecate
-def simple_nodes(G):
-    return [ n for n in list(G.nodes) if G.in_degree(n) == 1 and G.out_degree(n) == 1 ]
-
-# NOTE: deprecate
-def pop_nodes(G, ns):
-    """ pop out simple nodes, unless simple nodes form 3-clique.
-        3-clique may be formed when other simple nodes are popped out, thus it must be checked
-        just before trying to pop them out. """
-
-    for n in sorted(ns):
-        b = list(G.in_edges(n, keys = True, data = True))[0]
-        e = list(G.out_edges(n, keys = True, data = True))[0]
-
-        if (b[0], e[1]) in G.edges:
-            continue # triple
-
-        G.add_edge(b[0], e[1], key = b[2] + e[2])
-        # NOTE: too high ???
-        G.edges[b[0], e[1], b[2]+e[2]]["Score"] = b[3]["Score"] + e[3]["Score"]
-        G.remove_edge(b[0], n)
-        G.remove_edge(n, e[1])
-
-        if "dang" in G.nodes[b[0]]:
-            G.nodes[b[0]]["dang"] += [(n, b[2])]
-        else:
-            G.nodes[b[0]]["dang"] = [(n, b[2])]
-        if "dang" in G.nodes[n]:
-            G.nodes[b[0]]["dang"] += [ (m, b[2] + k) for m, k in G.nodes[n]["dang"] ]
-        G.remove_node(n)
-
-        # NOTE: equivalent to merge_nodes(G, b[0], n, b[2]) ?
-
 def merge_nodes(G, n, m, k):
     """ m is merged into n """
 
@@ -229,8 +162,9 @@ def merge_nodes(G, n, m, k):
         G.nodes[n]["dang"] += [ (l, k + lk) for l, lk in G.nodes[m]["dang"] ]
     G.remove_node(m)
 
-def prioritise_pair(G):
+def prioritise_pair(G, only_marked = False):
     """ require: G has no 3-clique. or should I check that here? (or blacklist might be passed?) """
+    """ in conservative mode, only confirmed edges can be picked to be reduced """
 
     def enumerate_triples(G): # e is a form of (s, t, k) or (s, t)
         triples = []
@@ -248,6 +182,8 @@ def prioritise_pair(G):
     el = []
     for n, m, k in G.edges(keys = True):
         if (n, m) in triples or (m, n) in triples:
+            continue
+        if only_marked and not "mark" in G.edges[n, m, k]:
             continue
         el += [ (G.edges[n, m, k]["Score"], n, m, k) ]
 
@@ -288,29 +224,34 @@ def remove_nontriple(G, n, m, k, arrs = None):
             print("got conflict l.289")
             G.remove_edge(n, m, k) # I dont wanna see you again.
             return -1 # without doing anything
-        # print(drydang)
 
     G.remove_edge(n, m, k)
     for mm, mj, mk in G.out_edges(m, keys = True):
-        G.add_edge(n, mj, key = k + mk) # TODO add data
+        G.add_edge(n, mj, key = k + mk)
         G.edges[n, mj, k + mk]["Score"] = G.edges[mm, mj, mk]["Score"]
+        if "mark" in G.edges[mm, mj, mk]:
+            G.edges[n, mj, k + mk]["mark"] = True 
     for mi, mm, mk in G.in_edges(m, keys = True):
         if mk - k >= 0:
-            G.add_edge(mi, n, key = mk - k) # TODO add data
+            G.add_edge(mi, n, key = mk - k)
             G.edges[mi, n, mk - k]["Score"] = G.edges[mi, mm, mk]["Score"]
+            if "mark" in G.edges[mi, mm, mk]:
+                G.edges[mi, n, mk - k]["mark"] = True 
         else:
-            G.add_edge(n, mi, key = k - mk) # TODO add data
+            G.add_edge(n, mi, key = k - mk)
             G.edges[n, mi, k - mk]["Score"] = G.edges[mi, mm, mk]["Score"]
+            if "mark" in G.edges[mi, mm, mk]:
+                G.edges[n, mi, k - mk]["mark"] = True 
 
     # TODO: record as dang of n
     merge_nodes(G, n, m, k)
 
-def remove_nontriple_all(G, arrs = None):
+def remove_nontriple_all(G, arrs = None, only_marked = False):
     # TODO make clear
-    n, e = 0, prioritise_pair(G)
+    n, e = 0, prioritise_pair(G, only_marked = only_marked)
     while e:
         remove_nontriple(G, e[1], e[2], e[3], arrs = arrs)
-        n, e = n + 1, prioritise_pair(G)
+        n, e = n + 1, prioritise_pair(G, only_marked = only_marked)
         print(".", end = "", flush = True)
 
     print(f"\nremoved {n} nodes")
@@ -325,19 +266,29 @@ def remove_transitives(G, cycles = None):
     # remove goods (longer edges will be gone)
     n = 0
     for i, j, k in cycles:
-        if (i, k) in G.edges(keys = False):
+        if (i, k) in G.edges(keys = False): # this condition may not be met because of preceeding ops.
             G.remove_edge(i, k)
             n += 1
-            # NOTE: to record data to existing edge, I need to consider the order of elimination
+
+        # NOTE: to record data to existing edge, I need to consider the order of elimination
+        # spec: i,j and j,k would be marked
+        if (i, j) in G.edges(keys = False):
+            kk = [ o for s, t, o in G.out_edges(i, keys = True) if t == j ][0]
+            G.edges[i, j, kk]["mark"] = True 
+        if (j, k) in G.edges(keys = False):
+            kk = [ o for s, t, o in G.out_edges(j, keys = True) if t == k ][0]
+            G.edges[j, k, kk]["mark"] = True 
+
     describe(G)
     return n
 
-def iter_tr(G, arrs = None):
+def iter_tr(G, arrs = None, only_marked = False):
     n = remove_transitives(G)
     while n > 0:
-        n = remove_nontriple_all(G, arrs = arrs)
+        n = remove_nontriple_all(G, arrs = arrs, only_marked = only_marked)
         n += remove_transitives(G)
 
+# NOTE: I won't use this now 
 def remove_intransitives(G, cycles = None):
     # remove bad edges based on its score
 
@@ -375,7 +326,7 @@ def limit_outedges(G):
     """ is this a final resort?? """
     pass
 
-def layout190512(G, prefix = "layouts", arrs = None):
+def layout190512(G, prefix = "layouts", arrs = None, only_marked = False):
     """ returns layout :: [(nodeid, pos)],
     which is then pickled and passed to visualization and stuff """
 
@@ -391,11 +342,28 @@ def layout190512(G, prefix = "layouts", arrs = None):
     # remove_intransitives(G)
 
     # then, iteratively reduce, edges & nodes ...
-    iter_tr(G, arrs = arrs)
+    iter_tr(G, arrs = arrs, only_marked = only_marked)
 
     nd = sorted(danglings(G, verbose = True),
-            key = lambda x: -x[1][-1][1]) # sort by the supposed genomic length
-    layouts = [ [(d[0], 0)] + d[1] for d in nd ]
+            key = lambda x: -x[1][-1][1]) # ?
+    layouts = [ sorted(
+        [(d[0], 0)] + d[1],
+        key = lambda x: x[1]) # sort by the supposed genomic length
+        for d in nd ] 
+
+    layouts = sorted(layouts, key = lambda x: x[-1][1] - x[0][1] + 1, reverse = True)
+
+    def ngx(ls, thre = 500):
+        # e.g., default: 500 is NG50 assuming 1000 units array
+        s = 0
+        for l in ls:
+            gl = l[-1][1] - l[0][1] + 1
+            s += gl
+            if s > thre:
+                return gl
+        return 1
+
+    print(f"NG25, 50, 75 = {ngx(layouts, 250)}, {ngx(layouts, 500)}, {ngx(layouts, 750)}")
     pickle.dump(layouts, open(prefix + ".pkl", "wb"))
 
     # then, check the resultant is OK as a set of layout.
@@ -417,6 +385,8 @@ if __name__ == '__main__':
     parser.add_argument('--prefix', dest='prefix', help='prefix of consensus read name')
     parser.add_argument('--noc', dest='noc', action='store_const', const=True,
             help='not allow conflict of special HOR vars in layouting')
+    parser.add_argument('--tri', dest='tri', action='store_const', const=True,
+            help='use only triangulated edges')
     parser.add_argument('-o', dest='outfile', help='the file to be output (consensus reads pickle)')
 
     args = parser.parse_args()
@@ -449,9 +419,17 @@ if __name__ == '__main__':
         # NOTE: maybe I want to change its logic a bit
         #layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}")
         if args.noc:
-            layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}-noc", arrs = arrs)
+            if args.tri:
+                layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}-noc-tri",
+                        arrs = arrs, only_marked = True)
+            else:
+                layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}-noc", arrs = arrs)
         else:
-            layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}", arrs = None)
+            if args.tri:
+                layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}-tri",
+                        arrs = None, only_marked = True)
+            else:
+                layout190512(G, prefix = datetimestr + f".{score_t}.{prom_t}.{round_t}", arrs = None)
 
         print("All done.")
 
